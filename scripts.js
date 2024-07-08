1c0fe13010c342cee57c7967c25e4256e38c92b7
Moxita Shah <143918404+moxitashahh@users.noreply.github.com>
2024-07-08 20:44:50 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#555';
+    } else {
+        navbar.style.backgroundColor = '#333';
+    }
+});
